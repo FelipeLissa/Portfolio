804c3df18c6dcca926b6a08ad76ce5aa7b9ec7d4
Felipe Fontoura Lissa <felipe.lissa@hotmail.com>
2022-07-19 14:40:57 -0300

    fixing imports

--- src/components/Conhecimentos/index.tsx
+++ src/components/Conhecimentos/index.tsx
@@ -1,8 +1,7 @@
 import { Container } from './styles';
 import SectionTitle from '../SectionTitle'
 import { AiFillHtml5 } from 'react-icons/ai';
-import { SiAdobephotoshop, SiAdobexd, SiAdobepremierepro, SiJavascript, SiNextdotjs, SiStyledcomponents } from 'react-icons/si';
-import {FaCss3Alt, FaReact} from 'react-icons/Fa';
+import { SiCss3,SiReact, SiAdobephotoshop, SiAdobexd, SiAdobepremierepro, SiJavascript, SiNextdotjs, SiStyledcomponents } from 'react-icons/si';
 import ConhecimentoItem from './ConhecimentoItem';
 
 export function Conhecimentos() {
@@ -11,9 +10,9 @@ export function Conhecimentos() {
       <SectionTitle title='Conhecimentos' children={[]}></SectionTitle>
       <section>
         <ConhecimentoItem title='HTML' icon={<AiFillHtml5/>}></ConhecimentoItem>
-        <ConhecimentoItem title='CSS' icon={<FaCss3Alt/>}></ConhecimentoItem>
+        <ConhecimentoItem title='CSS' icon={<SiCss3/>}></ConhecimentoItem>
         <ConhecimentoItem title='JavaScript' icon={<SiJavascript/>}></ConhecimentoItem>
-        <ConhecimentoItem title='React' icon={<FaReact/>}></ConhecimentoItem>
+        <ConhecimentoItem title='React' icon={<SiReact/>}></ConhecimentoItem>
         <ConhecimentoItem title='Next' icon={<SiNextdotjs/>}></ConhecimentoItem>
         <ConhecimentoItem title='Styled-Comp' icon={<SiStyledcomponents/>}></ConhecimentoItem>
         <ConhecimentoItem title='Adobe-Ps' icon={<SiAdobephotoshop/>}></ConhecimentoItem>
