0f2e15e19e70a562e23313002e0faa8f8974ba9b
Felipe Fontoura Lissa <felipe.lissa@hotmail.com>
2022-07-25 09:26:56 -0300

    solving key errors

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,22 +1,21 @@
 
 const nodemailer = require("nodemailer")
-import config from '../../../config.local';
 export default function sendEmail(req, res) {
     let transporter = nodemailer.createTransport ({
         host:'smtp-mail.outlook.com',
         secureConnection: false,
         port:'587',
         auth: {
-            user: config.USERMAIL,
-            pass:config.PASSMAIL
+            user: process.env.REACT_APP_USERMAIL,
+            pass:process.env.REACT_APP_PASSMAIL
         },
         tls: {
             ciphers:'SSLv3'
         }
     })
     transporter.sendMail({
-        from: config.USERMAIL, 
-        to: config.USERMAIL,
+        from: process.env.REACT_APP_USERMAIL,
+        to: process.env.REACT_APP_USERMAIL,
         subject: "Contato feito pelo Portfólio ✔",
         text: req.body.name+req.body.email+req.body.message,
         html:`<h1>${req.body.name}<h1/> <br/><h2> ${req.body.email}<h2/> <br/> <h2>${req.body.message}<h2/>`
